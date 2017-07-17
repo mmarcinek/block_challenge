a8ca1e708dffb146bb2becae6ab5d0af03465832
Michael Marcinek <michaelmarcinek@gmail.com>
2017-07-17 19:28:44 -0400

    updates JSON

--- src/App.js
+++ src/App.js
@@ -9,32 +9,39 @@ class App extends Component {
     var ITEMS = [
       { id: 1,
         topic: 'Embracing Discovery', 
-        content: "Every creative endeavor requires that you take risks. If you try and don't succeed, you've still learned something. It took Thomas Edison more than 10,000 tries to invent a viable lightbulb. You're not failing. You're discovering what doesn't work.", 
+        content: ["Wow, I am definitely a React.js convert! Honestly, this is the first project I have built in React(outside of some really interesting reading) and it was quite a joy to experience.",
+        "I feel like this gif accurately describes how I data moves through React via importing and extension of state and props from related components:"],
         url: 'https://media.giphy.com/media/l0HlFFTVOQz0bKYJG/giphy.gif'
       },
       { id: 2,
         topic: 'Gaining Insight', 
-        content: ["Gochujang kombucha mixtape heirloom fashion axe tofu. Ennui copper mug ethical hammock literally cloud bread, four dollar toast schlitz. Literally leggings live-edge neutra.  Shoreditch post-ironic fashion axe adaptogen aesthetic retro mlkshk echo park ennui. Snackwave lyft dreamcatcher, aesthetic activated charcoal vice lomo disrupt art party.",
-          "Here is a second paragraph that has its own value. Although we should probably consider mapping key from array indexes instead.",
-          "But, I can add a third and as long as the content is unique it will create a new node."
+        content: ["Take a look at the branch called 'tabs' on this repo. Here, I tried to dynamically generate the tabs and content through a fun functional map, spoiler alert, it failed to render properly. I really want to figure out how to solve the problem I was encountering with this methodology and have a guess.",
+        "To save time, I used npm to install material-ui for react, and well, by adding a <div> into the JSX render block, I am guessing the DOM node index is off for material-ui's tab mapping, so the flex container will not change direction to row and stacks vertically.",
+        "I am going to try breaking down the Tabs components into smaller parts instead of mapping the Tabs as one component, this will not solve my issue with node indexes, but hopefully it will make the custom CSS required to hack material-ui a bit more sane.",
+        "Hopefully you like what you see, and we will have a chance to talk about it, I would love to hear from someone with React experience just what it is I am doing wrong. :)"
         ], 
         url: ''
       },
       { id: 3,
         topic: 'Making it Real', 
-        content: ["Gochujang kombucha mixtape heirloom fashion axe tofu. Ennui copper mug ethical hammock literally cloud bread, four dollar toast schlitz. Literally leggings live-edge neutra.  Shoreditch post-ironic fashion axe adaptogen aesthetic retro mlkshk echo park ennui. Snackwave lyft dreamcatcher, aesthetic activated charcoal vice lomo disrupt art party.",
-        "This is my second paragraph on tab 3, which will automatically fit into the page."], 
-        url: 'https://media.giphy.com/media/l0HlFFTVOQz0bKYJG/giphy.gif'
+        content: ["So, yes, I love to write code! I find programming to be challenging, fun, and greatly rewarding!",
+        "I am also a visual artist, and that is what I studied in college. I really love photography, film(as in motion pictures, and yes, I still shoot film for stills as well as digital!), and printmaking. I like objects that are reproducable, I guess that is why I like programming too.",
+        "Most of my subject matter is related to the landscape and are serial documentary images. Here is an image from Boomtown, Pinedale Wyoming:"
+        ], 
+        url: 'http://payload201.cargocollective.com/1/11/363447/6328729/boomtown%202%20small.jpg'
       },
       { id: 4,
-        topic: 'Love the work', 
-        content: "Gochujang kombucha mixtape heirloom fashion axe tofu. Ennui copper mug ethical hammock literally cloud bread, four dollar toast schlitz. Literally leggings live-edge neutra.  Shoreditch post-ironic fashion axe adaptogen aesthetic retro mlkshk echo park ennui. Snackwave lyft dreamcatcher, aesthetic activated charcoal vice lomo disrupt art party.", 
-        url: ''
+        topic: 'Love the gifs', 
+        content: ["Did you all say Gif's? I absolutely love them! And well, cats!",
+        "Need I say more?"], 
+        url: 'http://www.somepets.com/wp-content/uploads/2013/09/hilarious-cat-gifs4.gif'
       },
       { id: 5,
         topic: 'Have Fun', 
-        content: "Gochujang kombucha mixtape heirloom fashion axe tofu. Ennui copper mug ethical hammock literally cloud bread, four dollar toast schlitz. Literally leggings live-edge neutra.  Shoreditch post-ironic fashion axe adaptogen aesthetic retro mlkshk echo park ennui. Snackwave lyft dreamcatcher, aesthetic activated charcoal vice lomo disrupt art party.", 
-        url: ''
+        content: ["And lots of coffee!", 
+        "I really enjoyed this challenge. It was really fun to play around in a different JavaScript Library for a while, especially one that is so well tuned for ES6!",
+        "I definitely could see getting a great dal of enjoyment out of learning React better, and really look forward to doing so."],
+        url: 'https://cdn.articulate.com/rise/courses/FtHG0DN2jjp0KHxN/q0r7xIVMCo4RkD5A.gif'
       }
     ]
 
