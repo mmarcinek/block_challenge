3f868977d694b572b647bfb7365f571fd967ea61
Michael Marcinek <michaelmarcinek@gmail.com>
2017-07-17 03:31:04 -0400

    builds and styles static-tabs"

--- src/BlockItem.js
+++ src/BlockItem.js
@@ -24,30 +24,54 @@ class BlockItem extends Component{
     ) 
   }
 
-  renderBlockItem(){  
-    return(
-      this.props.items.map(item =>(
-        <Tabs tabItemContainerStyle={{display: 'flex', whiteSpace: 'wrap'}} key={item.id} 
-              value={this.state.value}
-              onChange={this.handleChange = this.handleChange.bind(this)}>
-          <Tab buttonStyle={{flexDirection:'row'}} className={"tabs-container" + (this.state.value === item.id ? ' tabs-active' : ' tabs-inactive')} 
-               label={item.topic} 
-               value={item.id}>
-            <div className="tabs-content">
-              {(Array.isArray(item.content) ? this.contentArray(item.content) : item.content)}      
-              <img className="content-image" src={item.url} alt=''/>
-            </div> 
-          </Tab>
-        </Tabs>
-      ))
-    );
-  }
-
   render(){
     return(
-      <div className="BlockItem">
-        {this.renderBlockItem()}
-      </div>
+      <Tabs
+        tabItemContainerStyle={{display: 'flex', whiteSpace: 'wrap', height: 80 + 'px'}}
+        value={this.state.value}
+        onChange={this.handleChange = this.handleChange.bind(this)}
+      >
+        <Tab className={"tabs-container" + (this.state.value === this.props.items[0].id? ' tabs-active' : ' tabs-inactive')}
+             label={this.props.items[0].topic} 
+             value={this.props.items[0].id}>
+          <div className="tabs-content">
+            {(Array.isArray(this.props.items[0].content) ? this.contentArray(this.props.items[0].content) : this.props.items[0].content)}      
+            <img className="content-image" src={this.props.items[0].url} alt=''/>
+          </div> 
+        </Tab>
+        <Tab className={"tabs-container" + (this.state.value === this.props.items[1].id ? ' tabs-active' : ' tabs-inactive')}
+             label={this.props.items[1].topic} 
+             value={this.props.items[1].id}>
+          <div className="tabs-content">
+            {(Array.isArray(this.props.items[1].content) ? this.contentArray(this.props.items[1].content) : this.props.items[1].content)}      
+            <img className="content-image" src={this.props.items[1].url} alt=''/>
+          </div> 
+        </Tab>
+        <Tab className={"tabs-container" + (this.state.value === this.props.items[2].id ? ' tabs-active' : ' tabs-inactive')}
+             label={this.props.items[2].topic} 
+             value={this.props.items[2].id}>
+          <div className="tabs-content">
+            {(Array.isArray(this.props.items[2].content) ? this.contentArray(this.props.items[2].content) : this.props.items[2].content)}      
+            <img className="content-image" src={this.props.items[2].url} alt=''/>
+          </div> 
+        </Tab>
+        <Tab className={"tabs-container" + (this.state.value === this.props.items[3].id ? ' tabs-active' : ' tabs-inactive')} 
+             label={this.props.items[3].topic} 
+             value={this.props.items[3].id}>
+          <div className="tabs-content">
+            {(Array.isArray(this.props.items[3].content) ? this.contentArray(this.props.items[3].content) : this.props.items[3].content)}      
+            <img className="content-image" src={this.props.items[3].url} alt=''/>
+          </div> 
+        </Tab>
+        <Tab className={"tabs-container" + (this.state.value === this.props.items[4].id ? ' tabs-active' : ' tabs-inactive')}
+             label={this.props.items[4].topic} 
+             value={this.props.items[4].id}>
+          <div className="tabs-content">
+            {(Array.isArray(this.props.items[4].content) ? this.contentArray(this.props.items[4].content) : this.props.items[4].content)}      
+            <img className="content-image" src={this.props.items[4].url} alt=''/>
+          </div> 
+        </Tab>
+      </Tabs>
     )
   }
 }
